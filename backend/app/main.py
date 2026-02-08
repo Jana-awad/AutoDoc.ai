36a545721f94d9c370ef94817f3f0e3bac399f3b
Jana Awad <janasamiawad2005@gmail.com>
2026-02-08 18:35:08 +0200

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:8000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
