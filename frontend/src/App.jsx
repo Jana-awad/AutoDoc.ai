8fafed96f94d2f2f19f00ea958cdf447e2e9452e
Christina <christinazahreddine1209@gmail.com>
2026-03-06 19:53:52 +0200

    create the enterprise profile page frontend +backend (exact similar to business profile)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,11 @@ import TemplatesManager from "./pages/super/TemplatesManager";
 import EDashboard from "./pages/enterprise/Edashboard";
 import Template from "./pages/enterprise/template";
 import Api from "./pages/enterprise/api";
-import Profile from "./pages/enterprise/profile";
+import EnterpriseProfileLayout from "./pages/enterprise/profile/EnterpriseProfileLayout";
+import EAccountInfo from "./pages/enterprise/profile/EAccountInfo";
+import EManageUsers from "./pages/enterprise/profile/EManageUsers";
+import EBilling from "./pages/enterprise/profile/EBilling";
+import ESettings from "./pages/enterprise/profile/ESettings";
 import BDashboard from "./pages/business/B_dashboard";
 import BApi from "./pages/business/B_api";
 import BProfileLayout from "./pages/business/B_profile";
@@ -50,7 +54,13 @@ function App() {
           <Route path="/enterprise" element={<EDashboard />} />
           <Route path="/enterprise/template" element={<Template />} />
           <Route path="/enterprise/api" element={<Api />} />
-          <Route path="/enterprise/profile" element={<Profile />} />
+          <Route path="/enterprise/profile" element={<EnterpriseProfileLayout />}>
+            <Route index element={<EAccountInfo />} />
+            <Route path="account" element={<EAccountInfo />} />
+            <Route path="users" element={<EManageUsers />} />
+            <Route path="billing" element={<EBilling />} />
+            <Route path="settings" element={<ESettings />} />
+          </Route>
         </Route>
 
         <Route element={<ProtectedRoute allowedRoles={["business_admin"]} />}>
