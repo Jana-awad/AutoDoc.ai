be20885d2c4788486e6b9bf40c381b38ef740a1a
Christina <christinazahreddine1209@gmail.com>
2026-04-27 10:42:14 +0300

    comments

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
         default="postgresql+psycopg2://postgres:password@localhost:5432/autodoc"
     )
     UPLOAD_DIR: str = "uploads"  
-    SECRET_KEY: str = Field(default="CHANGE_ME")   # put real value in .env
-    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = Field(default="CHANGE_ME")   #  real value in .env
+    ALGORITHM: str = "HS256" #HMAC and Sha256 used for jwt token (symmetric w fast w simple)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     class Config:
         env_file = ".env"
